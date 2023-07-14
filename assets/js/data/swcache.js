0baf916ae6211df91c80ff0644ba04bbd979f5e0
x67sv <64111097+x67sv@users.noreply.github.com>
2023-07-14 03:58:18 +0300

    begin

---
+++ assets/js/data/swcache.js
@@ -0,0 +1,44 @@
+const resource = [
+    /* --- CSS --- */
+    '/assets/css/style.css',
+
+    /* --- PWA --- */
+    '/app.js',
+    '/sw.js',
+
+    /* --- HTML --- */
+    '/index.html',
+    '/404.html',
+
+    
+        '/categories/',
+    
+        '/tags/',
+    
+        '/archives/',
+    
+        '/about/',
+    
+
+    /* --- Favicons & compressed JS --- */
+    
+    
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+    
+
+    '192.168.1.40:4000',
+
+    
+
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net',
+    'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [];
+
