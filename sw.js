41d972776c0b77b9fb76327185e33c8f2832bb1a
x67sv <64111097+x67sv@users.noreply.github.com>
2023-08-25 16:14:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230824.223215';
+const cacheName = 'chirpy-20230825.074612';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
